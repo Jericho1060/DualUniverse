9a5d4a773e0d9ad59c96dae2882dcf92ecd26244
Thomas <jericho1060@gmail.com>
2020-09-30 17:56:52 +0200

    Update console text colors.lua
    fix typo

--- tools/console text colors.lua
+++ tools/console text colors.lua
@@ -4,7 +4,7 @@
 ]]
 system.printColor = function(message, color) system.print('<span style="color:' .. color .. ';">' .. message .. '</span>') end
 system.printPrimary = function(message) system.printColor(message, "#007bff") end
-system.printsecondary = function(message) system.printColor(message, "#6c757d") end
+system.printSecondary = function(message) system.printColor(message, "#6c757d") end
 system.printSuccess = function (message) system.printColor(message, "#28a745") end
 system.printDanger = function (message) system.printColor(message, "#dc3545") end
 system.printWarning = function (message) system.printColor(message, "#ffc107") end
